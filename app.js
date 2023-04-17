2f62bad97ff93a47ef9f13f91fc9f41bc1da988c
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-17 19:56:27 -0300

    connecting to database

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const app = require('./src/server');
+const connection = require('./src/database/connection');
 
+const { connectToDatabase } = connection;
 const { PORT } = process.env;
 
 (async () => {
-	// await connectToDatabase();
+	await connectToDatabase();
 
 	app.listen(PORT || 3000, async () => {
+		// eslint-disable-next-line
 		console.log(`App listening on port ${process.env.PORT}!`);
 	});
 })();
