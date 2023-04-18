4f7fc476a76e51eb61a4fcacbaccb5b39b1f5666
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-17 21:52:21 -0300

    create post task router

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const createTask = require('../middlewares/tasks/createTask');
+const { validateString, validateBoolean } = require('../middlewares/common/validations');
+const { NAME_FIELD, DESCRIPTION_FIELD, COMPLETED_FIELD } = require('../constants/task');
+const checkError = require('../middlewares/common/checkError');
 
 const taskRouter = express.Router();
 
 taskRouter.get('/', (req, res) => { res.json('Todo ok'); });
+
+taskRouter.post('/',
+	validateString(NAME_FIELD, true),
+	validateString(DESCRIPTION_FIELD, false),
+	validateBoolean(COMPLETED_FIELD, false),
+	checkError,
+	createTask);
 
 module.exports = taskRouter;
