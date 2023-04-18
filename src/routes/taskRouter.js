faa3d963984df97f30f00708f205b042db379135
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-17 23:32:46 -0300

    create delete task service and add to routes

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const createTask = require('../middlewares/tasks/createTask');
 const updateTask = require('../middlewares/tasks/updateTask');
 const getByIdTask = require('../middlewares/tasks/getByIdTask');
+const deleteTask = require('../middlewares/tasks/deleteTask');
 const { validateString, validateBoolean, validateId } = require('../middlewares/common/validations');
 const { NAME_FIELD, DESCRIPTION_FIELD, COMPLETED_FIELD } = require('../constants/task');
 const checkError = require('../middlewares/common/checkError');
@@ -31,6 +32,12 @@ taskRouter.get('/:id',
 	validateId,
 	checkError,
 	getByIdTask
+);
+
+taskRouter.delete('/:id',
+	validateId,
+	checkError,
+	deleteTask
 );
 
 module.exports = taskRouter;
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -3,5 +3,6 @@ const Task = require('../models/Task');
 const create = data => Task.create(data);
 const getById = id => Task.findById(id);
 const update = (task, data) => task.edit(data);
+const remove = id => Task.findOneAndDelete({ _id: id });
 
-module.exports = { create, getById, update };
+module.exports = { create, getById, update, remove };
