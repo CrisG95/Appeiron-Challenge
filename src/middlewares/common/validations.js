b0129fce047387ae1a5972d81980b90b2657647c
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-17 22:40:25 -0300

    validation mongo Id middleware

--- src/middlewares/common/validations.js
+++ src/middlewares/common/validations.js
@@ -1,4 +1,5 @@
 const validateString = require('./validateString');
 const validateBoolean = require('./validateBoolean');
+const validateId = require('./validateId');
 
-module.exports = { validateString, validateBoolean };
+module.exports = { validateString, validateBoolean, validateId };
