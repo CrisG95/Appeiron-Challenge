1b312a6c35f56a34d2470cd86eef3c586979b7fb
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-17 22:39:30 -0300

    add error codes

--- src/constants/errorCodes.js
+++ src/constants/errorCodes.js
@@ -1,3 +1,5 @@
 module.exports = {
-	INVALID_DATA: field => `DATA NOT SEND OR INVALID: '${field}'`
+	INVALID_DATA: field => `DATA NOT SEND OR INVALID: '${field}'`,
+	INVALID_ENTITY_ID: id => `INVALID ID: '${id}'`,
+	TASK_NOT_EXISTS: 'TASK NOT EXISTS'
 };
