01ee8c8c61f0d09df0a21140165f43b4d86cc2a3
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-18 22:56:18 -0300

    create validation number and validation sort middleware

--- src/middlewares/common/validations.js
+++ src/middlewares/common/validations.js
@@ -1,5 +1,9 @@
 const validateString = require('./validateString');
 const validateBoolean = require('./validateBoolean');
 const validateId = require('./validateId');
+const validateSort = require('./validateSort');
+const validateNumber = require('./validateNumber');
 
-module.exports = { validateString, validateBoolean, validateId };
+module.exports = {
+	validateString, validateBoolean, validateId, validateSort, validateNumber
+};
