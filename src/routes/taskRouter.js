b4b798f509894396c7135b4283cbc885dce2ed32
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-19 19:13:35 -0300

    add validation of authorization in routes

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -4,6 +4,7 @@ const updateTask = require('../middlewares/tasks/updateTask');
 const getByIdTask = require('../middlewares/tasks/getByIdTask');
 const deleteTask = require('../middlewares/tasks/deleteTask');
 const getTasks = require('../middlewares/tasks/getTasks');
+const isAuthorized = require('../middlewares/user/isAuthorized');
 
 const {
 	validateString, validateBoolean, validateId, validateSort, validateNumber
@@ -16,6 +17,7 @@ const checkError = require('../middlewares/common/checkError');
 const taskRouter = express.Router();
 
 taskRouter.post('/',
+	isAuthorized,
 	validateString(NAME_FIELD, true),
 	validateString(DESCRIPTION_FIELD, false),
 	validateBoolean(COMPLETED_FIELD, false),
@@ -24,6 +26,7 @@ taskRouter.post('/',
 );
 
 taskRouter.put('/:id',
+	isAuthorized,
 	validateString(NAME_FIELD, false),
 	validateString(DESCRIPTION_FIELD, false),
 	validateBoolean(COMPLETED_FIELD, false),
@@ -33,18 +36,21 @@ taskRouter.put('/:id',
 );
 
 taskRouter.get('/:id',
+	isAuthorized,
 	validateId,
 	checkError,
 	getByIdTask
 );
 
 taskRouter.delete('/:id',
+	isAuthorized,
 	validateId,
 	checkError,
 	deleteTask
 );
 
 taskRouter.get('/',
+	isAuthorized,
 	validateBoolean('status', false),
 	validateSort(NAME_FIELD, false),
 	validateSort(DESCRIPTION_FIELD, false),
