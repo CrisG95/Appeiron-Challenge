cc7d6bd38525df8913fd26ccee6f8814a0fde034
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-19 19:12:46 -0300

    add execution of seed db to app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const app = require('./src/server');
 const connection = require('./src/database/connection');
+const seedDB = require('./seed');
 
 const { connectToDatabase } = connection;
 const { PORT } = process.env;
 
 (async () => {
 	await connectToDatabase();
+	await seedDB();
 
-	app.listen(PORT || 3000, async () => {
-		// eslint-disable-next-line
-		console.log(`App listening on port ${process.env.PORT}!`);
-	});
 })();
 
-module.exports = app;
+const server = app.listen(PORT || 3000, async () => {
+	// eslint-disable-next-line
+	console.log(`App listening on port ${process.env.PORT}!`);
+});
+
+module.exports = server;
