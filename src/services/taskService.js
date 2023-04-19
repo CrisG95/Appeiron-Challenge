60b15ee3d385f1cf96f2148a06e23efb697eef13
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-18 22:56:49 -0300

    create task service get by filter

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -4,5 +4,8 @@ const create = data => Task.create(data);
 const getById = id => Task.findById(id);
 const update = (task, data) => task.edit(data);
 const remove = id => Task.findOneAndDelete({ _id: id });
+const get = ({ filter, sort, limit, page }) => Task.find(filter).sort(sort).limit(limit).skip(page);
 
-module.exports = { create, getById, update, remove };
+module.exports = {
+	create, getById, update, remove, get
+};
