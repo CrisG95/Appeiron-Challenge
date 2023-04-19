a8a48dac1097eebabb3781ab2320fd759a5bf099
Cristian Gonzalez <cgonzalez@moodtechnology.com>
2023-04-18 22:55:26 -0300

    add fields on task fields

--- src/constants/task.js
+++ src/constants/task.js
@@ -1,5 +1,7 @@
 module.exports = {
 	NAME_FIELD: 'name',
 	DESCRIPTION_FIELD: 'description',
-	COMPLETED_FIELD: 'completed'
+	COMPLETED_FIELD: 'completed',
+	LIMIT_FIELD: 'limit',
+	PAGE_FIELD: 'page'
 };
